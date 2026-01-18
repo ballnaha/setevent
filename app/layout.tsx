0e5ca1db510acaa2515ec4b13dc248a54fb7b6cc
RedPotion Developer <your-email@example.com>
2026-01-18 20:43:43 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,7 +110,9 @@ export const viewport: Viewport = {
   ],
   width: "device-width",
   initialScale: 1,
+  minimumScale: 1,
   maximumScale: 5,
+  userScalable: true,
   viewportFit: "cover",
 };
 
